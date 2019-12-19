51d363c11b2be9ccc03665554746e3710c24cbe4
Filip Hanik <fhanik@pivotal.io>
2019-12-18 18:13:54 -0800

    Add ability to send AuthNRequest via POST
    (XML Signature and Simple Signature)

--- saml2/saml2-service-provider/src/main/java/org/springframework/security/saml2/provider/service/servlet/filter/Saml2WebSsoAuthenticationRequestFilter.java
+++ saml2/saml2-service-provider/src/main/java/org/springframework/security/saml2/provider/service/servlet/filter/Saml2WebSsoAuthenticationRequestFilter.java
@@ -16,6 +16,7 @@
 
 package org.springframework.security.saml2.provider.service.servlet.filter;
 
+import org.springframework.http.MediaType;
 import org.springframework.security.saml2.credentials.Saml2X509Credential;
 import org.springframework.security.saml2.provider.service.authentication.OpenSamlAuthenticationRequestFactory;
 import org.springframework.security.saml2.provider.service.authentication.Saml2AuthenticationRequest;
@@ -34,7 +35,6 @@ import org.springframework.web.util.UriUtils;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.util.Collections;
-import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
@@ -44,7 +44,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import static java.lang.String.format;
-import static org.springframework.security.saml2.provider.service.registration.RelyingPartyRegistration.Saml2MessageBinding.POST;
 import static org.springframework.security.saml2.provider.service.registration.RelyingPartyRegistration.Saml2MessageBinding.REDIRECT;
 import static org.springframework.security.saml2.provider.service.registration.RelyingPartyRegistration.Saml2SignatureType.XML_SIGNATURE;
 import static org.springframework.security.saml2.provider.service.servlet.filter.Saml2Utils.deflate;
@@ -86,10 +85,10 @@ public class Saml2WebSsoAuthenticationRequestFilter extends OncePerRequestFilter
 		}
 
 		String registrationId = matcher.getVariables().get("registrationId");
-		sendRedirect(request, response, registrationId);
+		sendResponseData(request, response, registrationId);
 	}
 
-	private void sendRedirect(HttpServletRequest request, HttpServletResponse response, String registrationId)
+	private void sendResponseData(HttpServletRequest request, HttpServletResponse response, String registrationId)
 			throws IOException {
 		if (this.logger.isDebugEnabled()) {
 			this.logger.debug(format("Creating SAML2 SP Authentication Request for IDP[%s]", registrationId));
@@ -100,39 +99,47 @@ public class Saml2WebSsoAuthenticationRequestFilter extends OncePerRequestFilter
 			response.sendRedirect(redirectUrl);
 		}
 		else {
-			throw new UnsupportedOperationException(POST + " binding is not supported yet");
+			String formData = createSamlRequestFormData(request, relyingParty);
+			response.setContentType(MediaType.TEXT_HTML_VALUE);
+			response.getWriter().write(formData);
 		}
 	}
 
-	private String createSamlRequestRedirectUrl(HttpServletRequest request, RelyingPartyRegistration relyingParty) {
+	private RequestData getRequestParameters(
+			HttpServletRequest request,
+			RelyingPartyRegistration relyingParty,
+			boolean deflate) {
 		boolean sign = relyingParty.getIdpSsoConfiguration().isSignAuthNRequest();
 		boolean useXmlSig = relyingParty.getIdpSsoConfiguration().getSignatureType() == XML_SIGNATURE;
 		Saml2AuthenticationRequest authNRequest = createAuthenticationRequest(relyingParty, request, sign && useXmlSig);
 		String xml = this.authenticationRequestFactory.createAuthenticationRequest(authNRequest);
-		String encoded = encode(deflate(xml));
+		String encoded = deflate ? encode(deflate(xml)) : encode(xml.getBytes(StandardCharsets.UTF_8));
 		String relayState = request.getParameter("RelayState");
 		Map<String, String> queryParams = new LinkedHashMap<>();
-		queryParams.put("SAMLRequest", UriUtils.encode(encoded, StandardCharsets.ISO_8859_1));
+		if (deflate) {
+			queryParams.put("SAMLRequest", UriUtils.encode(encoded, StandardCharsets.ISO_8859_1));
+		}
+		else {
+			queryParams.put("SAMLRequest", encoded);
+		}
 		if (StringUtils.hasText(relayState)) {
 			queryParams.put("RelayState", UriUtils.encode(relayState, StandardCharsets.ISO_8859_1));
 		}
-
 		if (sign && !useXmlSig) {
 			//SimpleSignature
-			queryParams = this.authenticationRequestFactory.simpleSignSaml2Message(
+			Map<String, String> processed = this.authenticationRequestFactory.simpleSignSaml2Message(
 					queryParams,
 					relyingParty.getCredentials()
 			);
+			if (deflate) {
+				queryParams = processed;
+			}
+			else {
+				queryParams.put("Signature", processed.get("Signature"));
+				queryParams.put("SigAlg", processed.get("SigAlg"));
+			}
 		}
-
-		UriComponentsBuilder uriBuilder = UriComponentsBuilder.fromUriString(authNRequest.getDestination());
-		for (Map.Entry<String, String> entry : queryParams.entrySet()) {
-			uriBuilder.queryParam(entry.getKey(), entry.getValue());
-		}
-
-		return uriBuilder
-				.build(true)
-				.toUriString();
+		return new RequestData(authNRequest, queryParams);
 	}
 
 	private Saml2AuthenticationRequest createAuthenticationRequest(
@@ -155,6 +162,93 @@ public class Saml2WebSsoAuthenticationRequestFilter extends OncePerRequestFilter
 						)
 				)
 				.build();
+	}
+
+	private String createSamlRequestRedirectUrl(HttpServletRequest request, RelyingPartyRegistration relyingParty) {
+		RequestData data = getRequestParameters(request, relyingParty, true);
+
+		UriComponentsBuilder uriBuilder = UriComponentsBuilder.fromUriString(data.getRequest().getDestination());
+		for (Map.Entry<String, String> entry : data.getRequestParameters().entrySet()) {
+			uriBuilder.queryParam(entry.getKey(), entry.getValue());
+		}
+		return uriBuilder
+				.build(true)
+				.toUriString();
+	}
+
+	private String createSamlRequestFormData(HttpServletRequest request, RelyingPartyRegistration relyingParty) {
+		RequestData data = getRequestParameters(request, relyingParty, false);
+
+		StringBuilder postHtml = new StringBuilder()
+				.append("<!DOCTYPE html>\n")
+				.append("<html>\n")
+				.append("    <head>\n")
+				.append("        <meta charset=\"utf-8\" />\n")
+				.append("    </head>\n")
+				.append("    <body onload=\"document.forms[0].submit()\">\n")
+				.append("        <noscript>\n")
+				.append("            <p>\n")
+				.append("                <strong>Note:</strong> Since your browser does not support JavaScript,\n")
+				.append("                you must press the Continue button once to proceed.\n")
+				.append("            </p>\n")
+				.append("        </noscript>\n")
+				.append("        \n")
+				.append("        <form action=\"").append(data.getRequest().getDestination()).append("\" method=\"post\">\n")
+				.append("            <div>\n")
+				.append("                <input type=\"hidden\" name=\"SAMLRequest\" value=\"")
+				.append(data.getRequestParameters().get("SAMLRequest"))
+				.append("\"/>\n")
+				;
+		if (data.getRequestParameters().containsKey("RelayState")) {
+			postHtml
+					.append("                <input type=\"hidden\" name=\"RelayState\" value=\"")
+					.append(data.getRequestParameters().get("RelayState"))
+					.append("\"/>\n");
+		}
+		if (data.getRequestParameters().containsKey("SigAlg")) {
+			postHtml
+					.append("                <input type=\"hidden\" name=\"SigAlg\" value=\"")
+					.append(data.getRequestParameters().get("SigAlg"))
+					.append("\"/>\n");
+		}
+		if (data.getRequestParameters().containsKey("Signature")) {
+			postHtml
+					.append("                <input type=\"hidden\" name=\"Signature\" value=\"")
+					.append(data.getRequestParameters().get("Signature"))
+					.append("\"/>\n");
+		}
+		postHtml
+				.append("            </div>\n")
+				.append("            <noscript>\n")
+				.append("                <div>\n")
+				.append("                    <input type=\"submit\" value=\"Continue\"/>\n")
+				.append("                </div>\n")
+				.append("            </noscript>\n")
+				.append("        </form>\n")
+				.append("        \n")
+				.append("    </body>\n")
+				.append("</html>")
+				;
+		return postHtml.toString();
+	}
+
+	private class RequestData {
+		private final Saml2AuthenticationRequest request;
+		private final Map<String, String> requestParameters;
+
+		private RequestData(Saml2AuthenticationRequest request,
+				Map<String, String> requestParameters) {
+			this.request = request;
+			this.requestParameters = requestParameters;
+		}
+
+		public Saml2AuthenticationRequest getRequest() {
+			return request;
+		}
+
+		public Map<String, String> getRequestParameters() {
+			return requestParameters;
+		}
 	}
 
 }
