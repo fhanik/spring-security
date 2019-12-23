4b3354e99adb7d22651e3260005bdf86475bf2f5
Filip Hanik <fhanik@pivotal.io>
2019-12-23 14:01:14 -0800

    Order parameters, debug output

--- ci/tasks/gpg-create-signing-keyring.sh
+++ ci/tasks/gpg-create-signing-keyring.sh
@@ -2,11 +2,23 @@
 
 set -e -u -x
 
+BASEDIR=$(dirname "$0")
+
+echo "Creating GPG Public Key File"
 echo $1 > /tmp/spring-sec-gpg-public.key
+echo "Creating GPG Private Key File"
 echo $2 > /tmp/spring-sec-gpg-private.key
 
 cat /tmp/spring-sec-gpg-public.key
 cat /tmp/spring-sec-gpg-private.key
 
+echo "Import public key into keyring"
 gpg --no-default-keyring --keyring /tmp/trustedkeys.gpg --import /tmp/spring-sec-gpg-public.key
-echo $3 | gpg --batch --yes --no-default-keyring --keyring trustedkeys.gpg --import /tmp/spring-sec-gpg-private.key --yes --passphrase-fd 0
+echo "Import private key into keyring"
+echo $3 | gpg --batch --yes --no-default-keyring --keyring git-repo/trustedkeys.gpg --import /tmp/spring-sec-gpg-private.key --yes --passphrase-fd 0
+
+echo "Deleting temporary key files"
+rm -f /tmp/spring-sec-gpg-public.key
+rm -f /tmp/spring-sec-gpg-private.key
+
+echo "GPG keyring creation completed"
