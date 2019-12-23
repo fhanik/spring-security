1744a45927341763a57ef643de1a4f747001d272
Filip Hanik <fhanik@pivotal.io>
2019-12-23 14:36:40 -0800

    fix import command

--- ci/tasks/gpg-create-signing-keyring.sh
+++ ci/tasks/gpg-create-signing-keyring.sh
@@ -19,7 +19,7 @@ cat /tmp/spring-sec-gpg-private.key
 echo "Import public key into keyring"
 gpg --no-default-keyring --keyring $GIT_REPO/trustedkeys.gpg --import /tmp/spring-sec-gpg-public.key
 echo "Import private key into keyring"
-echo "$3" | gpg --batch --yes --no-default-keyring --keyring $GIT_REPO/trustedkeys.gpg --import /tmp/spring-sec-gpg-private.key --yes --passphrase-fd 0
+echo "$3" | gpg --yes --batch --no-default-keyring --keyring $GIT_REPO/trustedkeys.gpg --import /tmp/spring-sec-gpg-private.key
 
 echo "Deleting temporary key files"
 rm -f /tmp/spring-sec-gpg-public.key
